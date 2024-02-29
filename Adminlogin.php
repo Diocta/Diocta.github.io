6055a6e540ea4c4fa17b5e49abf66bceaea86995
Diocta <152760177+Diocta@users.noreply.github.com>
2024-02-29 22:57:40 +0700

    Add files via upload

---
+++ Adminlogin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="adminlogin.css">
+    <title>Admin Page</title>
+</head>
+<body>
+    <div class="box">
+        <div class="container">
+            <h1>Pilih karakter</h1>
+            <form action="">
+                <select name="" id="">
+                    <option value="">Yae Miko</option>
+                    <option value="">Furina</option>
+                    <option value="">Raiden</option>
+                    <option value="">Nahida</option>
+                </select>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ Userlogin.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Japanese Culture</title>
+  <script src="https://unpkg.com/feather-icons"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="jepangFIX.css">
+</head>
+<body style="background-color: black;">
+    <nav>
+      <img src="asset/Logo-removebg-preview.png" class="logo">
+      <div class="navbar-nav">
+        <a href="index.html">Home</a></li>
+        <a href="aboutjapan.html">About Japan</a></li>
+        <a href="japanculture.html">Japanese Culture</a></li>
+        <a href="Japan souvenirs.html">Japan Souvenirs</a></li>
+      </div>
+      <div class="navbar-icons">
+        <a href="#" id="menu-icons"><i data-feather="menu"></i></a>
+      </div>
+    </nav>
+    <section class="hero">
+      <main class="content">
+        <h1>Mari Lihat Budaya <span>Jepang</span></h1>
+        <p>DiJepang terdapat berbagai macam budaya yang unik dan juga keren. </p>
+        <a href="aboutjapan.html" class="tombol">SEE MORE</a>
+      </main>
+    </section>
+    <footer style="text-align: center; padding: 1rem 0; background-color: rgb(255, 200, 255); background: linear-gradient(to top, rgba(200, 200, 255, 255), white 88%, rgba(0, 0, 0, 0));">
+      <div class="sosial" style="margin-top: 1rem;">
+        <a href="https://www.instagram.com/dioctabri?igsh=Njc4NmN5ZHl0NjBh" style="margin: 1rem; "onmouseover="this.style.color='red'" onmouseout="this.style.color='blue'""><i data-feather="instagram"></i></a>
+        <a href="https://x.com/XdRiyoo?t=7zqFvAYolJNq-CGptMMrNg&s=09" style="margin: 1rem; "onmouseover="this.style.color='red'" onmouseout="this.style.color='blue'""><i data-feather="twitter"></i></a>
+        <a href="#" style="margin: 1rem; "onmouseover="this.style.color='red'" onmouseout="this.style.color='blue'""><i data-feather="facebook"></i></a>
+        <a href="https://youtube.com/@Riyooo31?si=Ngs6nwpSPYvkVQtA" style="margin: 1rem; "onmouseover="this.style.color='red'" onmouseout="this.style.color='blue'""><i data-feather="youtube"></i></a>
+      </div>
+
+      <div class="link">
+        <a href="index.html" style="color: red; text-decoration: none;" onmouseover="this.style.color='blue'" onmouseout="this.style.color='red'">Home</a>
+        <a href="aboutjapan.html" style="color: red; text-decoration: none;" onmouseover="this.style.color='blue'" onmouseout="this.style.color='red'">About Japan</a>
+        <a href="japanculture.html" style="color: red; text-decoration: none;" onmouseover="this.style.color='blue'" onmouseout="this.style.color='red'">Culture</a>
+      </div>
+
+      <div class="credit">
+        <p style="font-weight: bold;">Created by <a href="profill.html" style="color: blue; font-weight: bold; text-decoration: none;" onmouseover="this.style.color='red'" onmouseout="this.style.color='blue'">DioctaBrianCesario</a>. | &copy; 2024.</p>
+      </div>
+    </footer>
+  <script>
+    feather.replace();
+  </script>
+  <script src="jepangFIX.js"></script>
+</body>
+</html>
